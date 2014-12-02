6e659c58e43f7485bd9357aaa9d535913ef9d800
dbuscombe-usgs <dbuscombe@usgs.gov>
2014-12-01 18:00:37 -0700

    Mon Dec  1 18:00:37 MST 2014

--- PyHum/__init__.py
+++ PyHum/__init__.py
@@ -22,7 +22,7 @@ For more information visit http://dbuscombe-usgs.github.io/PyHum/
     
 """
 
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 #-----------------------------------------------------------------------------
 # Imports
