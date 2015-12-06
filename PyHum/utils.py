490f0d1606dd154d484ac64c3c72fa569e7e6308
dbuscombe-usgs <dbuscombe@usgs.gov>
2015-12-06 02:06:58 -0700

    Sun Dec  6 02:06:58 MST 2015

--- PyHum/utils.py
+++ PyHum/utils.py
@@ -375,7 +375,8 @@ def sliding_window(a,ws,ss = None,flatten = True):
       # Allocate memory to hold all valid n-dimensional slices
       nslices = np.product([len(s) for s in slices])
       #out = np.ndarray((nslices,) + tuple(ws),dtype = a.dtype)
-      out = np.empty((nslices,) + tuple(ws),dtype = a.dtype)      
+      #out = np.empty((nslices,) + tuple(ws),dtype = a.dtype) 
+      out = da.random.normal(10, 0.1, size=(nslices,) + tuple(ws), chunks=(1000))
       for i,s in enumerate(allslices):
          out[i] = a[s]
          
