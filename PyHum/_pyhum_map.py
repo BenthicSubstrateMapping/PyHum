a966a6eea671aac4f7a70d1451e13b1aa7ee316a
dbuscombe-usgs <dbuscombe@usgs.gov>
2016-01-10 00:50:19 -0700

    Sun Jan 10 00:50:19 MST 2016

--- PyHum/_pyhum_map.py
+++ PyHum/_pyhum_map.py
@@ -628,17 +628,17 @@ def getgrid_lm(humlon, humlat, merge, influence, minX, maxX, minY, maxY, res, mo
    return dat, stdev, counts, res, complete, shape
 
 
-# =========================================================
-def getxy(e, n, yvec, d, t,extent):
-   x = np.concatenate((np.tile(e,extent) , np.tile(e,extent)))
-   rangedist = np.sqrt(np.power(yvec, 2.0) - np.power(d, 2.0))
-   y = np.concatenate((n+rangedist, n-rangedist))
-   # Rotate line around center point
-   xx = e - ((x - e) * np.cos(t)) - ((y - n) * np.sin(t))
-   yy = n - ((x - e) * np.sin(t)) + ((y - n) * np.cos(t))
-   xx, yy = calc_beam_pos(d, t, xx, yy)
-   #x, y, eucl. dist, depth, theta 
-   return xx, yy, np.sqrt((xx-e)**2 + (yy-n)**2), np.ones(len(xx))*d, np.ones(len(xx))*t
+## =========================================================
+#def getxy(e, n, yvec, d, t,extent):
+#   x = np.concatenate((np.tile(e,extent) , np.tile(e,extent)))
+#   rangedist = np.sqrt(np.power(yvec, 2.0) - np.power(d, 2.0))
+#   y = np.concatenate((n+rangedist, n-rangedist))
+#   # Rotate line around center point
+#   xx = e - ((x - e) * np.cos(t)) - ((y - n) * np.sin(t))
+#   yy = n - ((x - e) * np.sin(t)) + ((y - n) * np.cos(t))
+#   xx, yy = calc_beam_pos(d, t, xx, yy)
+#   #x, y, eucl. dist, depth, theta 
+#   return xx, yy, np.sqrt((xx-e)**2 + (yy-n)**2), np.ones(len(xx))*d, np.ones(len(xx))*t
 
 
 # =========================================================
