7da2f409d25bbbaf361cf81b847311d5cfb7238a
Daniel Buscombe <dbuscombe@usgs.gov>
2016-03-12 16:52:46 -0700

    Sat Mar 12 16:52:46 MST 2016

--- setup.py
+++ setup.py
@@ -175,7 +175,7 @@ else:
         Extension('_RunningStats',sources=['PyHum/RunningStats_wrap.cxx', 'PyHum/RunningStats.cpp']),
     ]
 install_requires = [
-    'numpy','scipy>=0.16','Pillow','matplotlib', 'cython', 'pyproj', 'scikit-image', 'simplekml', 'joblib==0.7.1', 'basemap', 'scikit-learn', 'pyresample', 'dask', 'toolz', 'pandas'
+    'numpy','scipy==0.16.0','Pillow','matplotlib', 'cython', 'pyproj', 'scikit-image', 'simplekml', 'joblib==0.7.1', 'basemap', 'scikit-learn', 'pyresample', 'dask', 'toolz', 'pandas'
 ]
 
 #long_description = open('README.md').read()
