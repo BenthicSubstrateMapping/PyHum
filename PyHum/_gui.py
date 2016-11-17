a9827981651fb58fc03df3a018876152e6d43f8a
Daniel Buscombe <dbuscombe@usgs.gov>
2016-11-17 16:43:13 -0700

    Thu Nov 17 16:43:13 MST 2016

--- PyHum/_gui.py
+++ PyHum/_gui.py
@@ -420,8 +420,8 @@ def gui():
 	    PyHum.read(self.DATfilename.get(), os.path.dirname(self.SONfiles[0]), str(self.epsg.get()), self.cvar.get(), self.dvar.get(), self.doplot, self.tvar.get(), self.bedpick, self.flipvar.get(), self.model, self.calcheadvar.get(), self.filtheadvar.get(), self.chunk)
 
 
-	    #self.update()
-	    #tkMessageBox.showinfo("Done!", "Read module finished") 
+	    self.update()
+	    tkMessageBox.showinfo("Done!", "Read module finished") 
 
 	#=======================        
 	def _get_DAT(master, v):
