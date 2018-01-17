fbb459d807fac9c5d80322d1a91826a70353dd9c
Daniel Buscombe <dbuscombe@usgs.gov>
2018-01-17 12:51:16 -0700

    Wed Jan 17 12:51:16 MST 2018

--- PyHum/__init__.py
+++ PyHum/__init__.py
@@ -69,7 +69,7 @@ from PyHum.test import *
 from PyHum.io import *
 from PyHum._pyhum_mosaic import mosaic
 from PyHum._pyhum_mosaic_texture import mosaic_texture
-from PyHum._gui import gui
+#from PyHum._gui import gui
 
 from PyHum._pyhum_texture import texture
 from PyHum._pyhum_texture_slic import texture_slic
